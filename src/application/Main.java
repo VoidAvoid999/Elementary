a0af39777021e635a95f7503fa9d5d876c9cbb72
VoidAvoid999 <292600575+VoidAvoid999@users.noreply.github.com>
2026-06-25 15:04:52 -0600

    Messing with stage properties.

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,25 +2,39 @@ package application;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 
+// stage is like the window
+// scene is what we add stuff to (resizes automatically)
+// nodes are components that we add
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+	
 	
 	public static void main(String[] args) {
-		launch(args);
+		launch(args); //static method
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		
+		Group root = new Group(); // grouping of nodes
+		Scene scene = new Scene(root, Color.BLACK); // needs root node
+		
+		Image icon = new Image("icon.png"); // icon in src
+		stage.getIcons().add(icon);
+		stage.setTitle("Elementry Calculator");
+		stage.setWidth(420); // in pixels
+		stage.setHeight(420);
+		//stage.setResizable(false);
+		stage.setX(0);
+		stage.setY(50); // + = down, - = up
+		
+		stage.setScene(scene); // set the scene
+		stage.show(); // show the stage
 	}
 }
