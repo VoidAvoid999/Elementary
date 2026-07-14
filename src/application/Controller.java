de2680ccdffdaeb361529fde2e488f6e6da1805d
VoidAvoid999 <292600575+VoidAvoid999@users.noreply.github.com>
2026-07-14 11:11:19 -0600

    Created calculator layout and setup button functionality.

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,34 +1,114 @@
 package application;
 
+import java.util.ArrayList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.shape.Arc;
 
 public class Controller {
 	
 	@FXML // this annotation injects the values in Main.fxml into this class
-	private Arc pacMan;
-	private double x;
-	private double y;
 	
-	public void up(ActionEvent e) {
-		System.out.println("Up Up Up!");
-		pacMan.setCenterY(y-=10);  
+	private byte position = -1;
+	private double result = 0.0;
+	
+	private ArrayList<Double> input = new ArrayList<>();
+	
+	public void addition(ActionEvent e) {
+		System.out.println("+");
 	}
 	
-	public void down(ActionEvent e) {
-		System.out.println("Down Down Down!");
-		pacMan.setCenterY(y+=10);  
+	public void subtraction(ActionEvent e) {
+		System.out.println("-");
 	}
 	
-	public void left(ActionEvent e) {
-		System.out.println("Left Left Left!");
-		pacMan.setCenterX(x-=10);  
+	public void multiplication(ActionEvent e) {
+		System.out.println("*");
 	}
 	
-	public void right(ActionEvent e) {
-		System.out.println("Right Right Right!");
-		pacMan.setCenterX(x+=10);  
+	public void division(ActionEvent e) {
+		System.out.println("/");
+	}
+	
+	public void equals(ActionEvent e) {
+		System.out.println(" = " + result);
+		input.clear();
+		position = -1;
+		
+	}
+	
+	public void decimal(ActionEvent e) {
+		System.out.println(".");
+		input.set(position, null);
+	}
+	
+	public void zero(ActionEvent e) {
+		position++;
+		System.out.println("position = " + position);
+		input.add(0.0);
+		System.out.println("value = " + input.get(position)); 
+	}
+	
+	public void one(ActionEvent e) {
+		position++;
+		System.out.println("position = " + position);
+		input.add(1.0);
+		System.out.println("value = " + input.get(position));
+	}
+	
+	public void two(ActionEvent e) {
+		position++;
+		System.out.println("position = " + position);
+		input.add(2.0);
+		System.out.println("value = " + input.get(position));
+	}
+	
+	public void three(ActionEvent e) {
+		position++;
+		System.out.println("position = " + position);
+		input.add(3.0);
+		System.out.println("value = " + input.get(position));
+	}
+	
+	public void four(ActionEvent e) {
+		position++;
+		System.out.println("position = " + position);
+		input.add(4.0);
+		System.out.println("value = " + input.get(position));
+	}
+	
+	public void five(ActionEvent e) {
+		position++;
+		System.out.println("position = " + position);
+		input.add(5.0);
+		System.out.println("value = " + input.get(position));
+	}
+	
+	public void six(ActionEvent e) {
+		position++;
+		System.out.println("position = " + position);
+		input.add(6.0);
+		System.out.println("value = " + input.get(position));
+	}
+	
+	public void seven(ActionEvent e) {
+		position++;
+		System.out.println("position = " + position);
+		input.add(7.0);
+		System.out.println("value = " + input.get(position));
+	}
+	
+	public void eight(ActionEvent e) {
+		position++;
+		System.out.println("position = " + position);
+		input.add(8.0);
+		System.out.println("value = " + input.get(position));
+	}
+	
+	public void nine(ActionEvent e) {
+		position++;
+		System.out.println("position = " + position);
+		input.add(9.0);
+		System.out.println("value = " + input.get(position));
 	}
 	
 }
